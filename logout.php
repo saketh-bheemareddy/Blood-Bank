cf97a2e6aad81febd03f2816de6e6554f8408918
saketh-bheemareddy <saketh.naidu.15@gmail.com>
2022-05-21 22:06:17 +0530

    group and location pages bootstrap changes

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
 session_destroy();
-echo "successfully logged out <br> <br>";
-echo "<a href='index.php'>Click here to go to Home page</a>"
+header("Location:login.php");
 ?>
